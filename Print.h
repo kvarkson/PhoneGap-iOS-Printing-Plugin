884d7992aa00717fa2302a5fc405379105ba42af
kvarkson <kvarkson@gmail.com>
2013-10-17 19:57:43 +0300

    initial commit

---
+++ Print.h
@@ -0,0 +1,33 @@
+//
+//  PrintPlugin.m
+//  Print Plugin
+//
+//  Created by Stas Gorodnichenko on 15/10/2011.
+//  Copyright 2013 Stas Gorodnichenko. All rights reserved.
+//  MIT licensed
+//
+
+#import <Foundation/Foundation.h>
+
+
+#import <Cordova/CDVPlugin.h>
+
+
+@interface Print : CDVPlugin
+    
+@property (nonatomic, strong) NSString* successCallback;
+@property (nonatomic, strong) NSString* failCallback;
+@property (nonatomic, strong) NSString* printHTML;
+
+//Print Settings
+@property NSInteger dialogLeftPos;
+@property NSInteger dialogTopPos;
+
+//Print HTML
+- (void)print:(CDVInvokedUrlCommand *)command;
+- (void)printWithArgs:(NSDictionary*)arguments;
+
+//Find out whether printing is supported on this platform.
+- (void) isPrintingAvailable:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+@end
---
+++ Print.js
@@ -0,0 +1,65 @@
+/**
+ * Printer Plugin
+ * Copyright (c) 2013 Stas Gorodnichenko
+ * MIT licensed
+ */
+
+var Print =  {
+
+    callbackMap: {},
+    callbackIdx: 0,
+
+    /*
+     print      - html string or DOM node (if latter, innerHTML is used to get the contents). REQUIRED.
+     success    - callback function called if print successful.     {success: true}
+     fail       - callback function called if print unsuccessful.  If print fails, {error: reason}. If printing not available: {available: false}
+     options    -  {dialogOffset:{left: 0, right: 0}}. Position of popup dialog (iPad only).
+     */
+    print: function(printHTML, success, fail, options) 
+    {
+        if (typeof printHTML != 'string'){
+            console.log("Print function requires an HTML string. Not an object");
+            return;
+        }
+        //var printHTML = "";
+        var dialogLeftPos = 0;
+        var dialogTopPos = 0;
+
+        if (options){
+            if (options.dialogOffset){
+                if (options.dialogOffset.left){
+                    dialogLeftPos = options.dialogOffset.left;
+                    if (isNaN(dialogLeftPos)){
+                        dialogLeftPos = 0;
+                    }
+                }
+                if (options.dialogOffset.top){
+                    dialogTopPos = options.dialogOffset.top;
+                    if (isNaN(dialogTopPos)){
+                        dialogTopPos = 0;
+                    }
+                }
+            }
+        }
+
+        args = {}
+        args.printHTML = printHTML;
+        args.dialogLeftPos = dialogLeftPos;
+        args.dialogTopPos = dialogTopPos;
+        cordova.exec( null, null, "Print", "print", [args] );
+    },
+
+    /*
+     * Callback function returns {available: true/false}
+     */
+    isPrintingAvailable: function(callback) {
+        var key = 'isPrintingAvailable' + this.callbackIdx++;
+        Print.callbackMap[key] = function(result) {
+            delete Print.callbackMap[key];
+            callback(result);
+        };
+
+        var callbackName = 'Print.callbackMap.' + key;
+        cordova.exec("Print.isPrintingAvailable", callbackName);
+    }
+}
